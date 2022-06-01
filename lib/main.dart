4ebfa3a08b38e85cb9459239128c81989e18d60a
Craig <craig@oppkey.com>
2022-06-01 06:22:46 -0700

    completed world bounds

--- lib/main.dart
+++ lib/main.dart
@@ -28,11 +28,14 @@ class ChickenGame extends FlameGame with HasDraggables {
     await super.onLoad();
     print('2. load the assets for the game');
 
+    print('3. load map');
     var homeMap = await TiledComponent.load('level_1.tmx', Vector2(16, 16));
+    print('4. add map to game');
     add(homeMap);
     double mapHeight = 16.0 * homeMap.tileMap.map.height;
 
     camera.viewport = FixedResolutionViewport(Vector2(1280, mapHeight));
+    print('5. load charlie chicken image');
     Image chickenImage = await images.load('chicken.png');
     var chickenAnimation = SpriteAnimation.fromFrameData(
         chickenImage,
@@ -57,7 +60,23 @@ class ChickenGame extends FlameGame with HasDraggables {
   @override
   void update(double dt) {
     super.update(dt);
-    chicken.position.add(joystick.relativeDelta * 300 * dt);
+    bool moveLeft = joystick.relativeDelta[0] < 0;
+    bool moveRight = joystick.relativeDelta[0] > 0;
+    bool moveUp = joystick.relativeDelta[1] < 0;
+    bool moveDown = joystick.relativeDelta[1] > 0;
+    double chickenVectorX = (joystick.relativeDelta * 300 * dt)[0];
+    double chickenVectorY = (joystick.relativeDelta * 300 * dt)[1];
+
+    // chicken is moving horizontally
+    if ((moveLeft && chicken.x > 0) || (moveRight && chicken.x < size[0])) {
+      chicken.position.add(Vector2(chickenVectorX, 0));
+    }
+    // chicken is moving vertically
+    if ((moveUp && chicken.y > 0) ||
+        (moveDown && chicken.y < size[1] - chicken.height)) {
+      chicken.position.add(Vector2(0, chickenVectorY));
+    }
+
     if (joystick.relativeDelta[0] < 0 && chickenFlipped) {
       chickenFlipped = false;
       chicken.flipHorizontallyAroundCenter();
