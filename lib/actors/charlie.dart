c0c3361b983190014efeaa5131c2865695740eea
Craig <craig@oppkey.com>
2022-06-12 18:16:28 -0700

    collision detection for obstacles all four directions work

--- lib/actors/charlie.dart
+++ lib/actors/charlie.dart
@@ -2,9 +2,13 @@ import 'package:charlie_chicken/main.dart';
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 
+import '../world/obstacle.dart';
+
 class Charlie extends SpriteAnimationComponent
     with CollisionCallbacks, HasGameRef<ChickenGame> {
   bool chickenFlipped = false;
+  bool collided = false;
+  JoystickDirection collidedDirection = JoystickDirection.idle;
 
   Future<void> onLoad() async {
     await super.onLoad();
@@ -15,20 +19,38 @@ class Charlie extends SpriteAnimationComponent
   @override
   void update(double dt) {
     super.update(dt);
-    bool moveLeft = gameRef.joystick.relativeDelta[0] < 0;
-    bool moveRight = gameRef.joystick.relativeDelta[0] > 0;
-    bool moveUp = gameRef.joystick.relativeDelta[1] < 0;
-    bool moveDown = gameRef.joystick.relativeDelta[1] > 0;
+    bool moveLeft = gameRef.joystick.direction == JoystickDirection.left;
+    bool moveRight = gameRef.joystick.direction == JoystickDirection.right;
+    bool moveUp = gameRef.joystick.direction == JoystickDirection.up;
+    bool moveDown = gameRef.joystick.direction == JoystickDirection.down;
     double chickenVectorX = (gameRef.joystick.relativeDelta * 300 * dt)[0];
     double chickenVectorY = (gameRef.joystick.relativeDelta * 300 * dt)[1];
 
-    // chicken is moving horizontally
-    if ((moveLeft && x > 0) || (moveRight && x < gameRef.size[0])) {
-      position.add(Vector2(chickenVectorX, 0));
+    // chicken is moving left
+    if (moveLeft && x > 0) {
+      if (!collided || collidedDirection == JoystickDirection.right) {
+        x += chickenVectorX;
+      }
     }
-    // chicken is moving vertically
-    if ((moveUp && y > 0) || (moveDown && y < gameRef.size[1] - height)) {
-      position.add(Vector2(0, chickenVectorY));
+// chicken is moving right
+    if (moveRight && x < gameRef.size[0]) {
+      if (!collided || collidedDirection == JoystickDirection.left) {
+        x += chickenVectorX;
+      }
+    }
+
+    // chicken s moving up
+    if (moveUp && y > 0) {
+      if (!collided || collidedDirection == JoystickDirection.down) {
+        y += chickenVectorY;
+      }
+    }
+
+    // chicken s moving down
+    if (moveDown && y < gameRef.size[1] - height) {
+      if (!collided || collidedDirection == JoystickDirection.up) {
+        y += chickenVectorY;
+      }
     }
 
     if (gameRef.joystick.relativeDelta[0] < 0 && chickenFlipped) {
@@ -44,7 +66,19 @@ class Charlie extends SpriteAnimationComponent
 
   @override
   void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
-    // TODO: implement onCollision
     super.onCollision(intersectionPoints, other);
+    if (other is Obstacle) {
+      if (!collided) {
+        collided = true;
+        collidedDirection = gameRef.joystick.direction;
+      }
+    }
+  }
+
+  @override
+  void onCollisionEnd(PositionComponent other) {
+    super.onCollisionEnd(other);
+    collidedDirection = JoystickDirection.idle;
+    collided = false;
   }
 }
